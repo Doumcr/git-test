a7d1e832809ba95348c6557bbefc570596e47d26
fan <jiajunit@sina.com>
2017-04-10 00:23:48 +0800

    '第一次提交代码到本地仓库'

---
+++ js/main.js
@@ -0,0 +1,424 @@
+$(function(){
+    $("#icon-keep").on({
+        mouseover:function(){
+            $(".icon-move-one").show();
+        },
+        mouseout:function(){
+            $(".icon-move-one").hide();
+        }
+    });
+    $("#icon-carry").on({
+        mouseover:function(){
+            $(".icon-move-two").show();
+        },
+        mouseout:function(){
+            $(".icon-move-two").hide();
+        }
+    });
+    $("#icon-vip").on({
+        mouseover:function(){
+            $(".icon-move-three").show();
+        },
+        mouseout:function(){
+            $(".icon-move-three").hide();
+        }
+    });
+
+    $("#icon-keep-high").on({
+        mouseover:function(){
+            $(".high-move-one").show();
+        },
+        mouseout:function(){
+            $(".high-move-one").hide();
+        }
+    });
+    $("#icon-carry-high").on({
+        mouseover:function(){
+            $(".high-move-two").show();
+        },
+        mouseout:function(){
+            $(".high-move-two").hide();
+        }
+    });
+    $("#icon-is-high").on({
+        mouseover:function(){
+            $(".high-move-three").show();
+        },
+        mouseout:function(){
+            $(".high-move-three").hide();
+        }
+    });
+    $("#icon-vip-high").on({
+        mouseover:function(){
+            $(".high-move-four").show();
+        },
+        mouseout:function(){
+            $(".high-move-four").hide();
+        }
+    });
+
+    $("#icon-keep-home").on({
+        mouseover:function(){
+            $(".home-move-one").show();
+        },
+        mouseout:function(){
+            $(".home-move-one").hide();
+        }
+    });
+    $("#icon-carry-home").on({
+        mouseover:function(){
+            $(".home-move-two").show();
+        },
+        mouseout:function(){
+            $(".home-move-two").hide();
+        }
+    });
+    $("#icon-is-home").on({
+        mouseover:function(){
+            $(".home-move-three").show();
+        },
+        mouseout:function(){
+            $(".home-move-three").hide();
+        }
+    });
+
+    $("#keep").on({
+        mouseover:function(){
+            $(".icon-oneh").show();
+        },
+        mouseout:function(){
+            $(".icon-oneh").hide();
+        }
+    });
+    $("#bear").on({
+        mouseover:function(){
+            $(".icon-twoh").show();
+        },
+        mouseout:function(){
+            $(".icon-twoh").hide();
+        }
+    });
+    $("#is").on({
+        mouseover:function(){
+            $(".icon-threeh").show();
+        },
+        mouseout:function(){
+            $(".icon-threeh").hide();
+        }
+    });
+    $("#vip").on({
+        mouseover:function(){
+            $(".icon-fourh").show();
+        },
+        mouseout:function(){
+            $(".icon-fourh").hide();
+        }
+    });
+
+
+    $("#keepo").on({
+        mouseover:function(){
+            $(".icon-oneo").show();
+        },
+        mouseout:function(){
+            $(".icon-oneo").hide();
+        }
+    });
+    $("#bearo").on({
+        mouseover:function(){
+            $(".icon-twoo").show();
+        },
+        mouseout:function(){
+            $(".icon-twoo").hide();
+        }
+    });
+    $("#iso").on({
+        mouseover:function(){
+            $(".icon-threeo").show();
+        },
+        mouseout:function(){
+            $(".icon-threeo").hide();
+        }
+    });
+    $("#vipo").on({
+        mouseover:function(){
+            $(".icon-fouro").show();
+        },
+        mouseout:function(){
+            $(".icon-fouro").hide();
+        }
+    });
+
+    $("#keeps").on({
+        mouseover:function(){
+            $(".icon-ones").show();
+        },
+        mouseout:function(){
+            $(".icon-ones").hide();
+        }
+    });
+    $("#bears").on({
+        mouseover:function(){
+            $(".icon-twos").show();
+        },
+        mouseout:function(){
+            $(".icon-twos").hide();
+        }
+    });
+    $("#iss").on({
+        mouseover:function(){
+            $(".icon-threes").show();
+        },
+        mouseout:function(){
+            $(".icon-threes").hide();
+        }
+    });
+
+
+    $("#keepl").on({
+        mouseover:function(){
+            $(".icon-onel").show();
+        },
+        mouseout:function(){
+            $(".icon-onel").hide();
+        }
+    });
+    $("#bearl").on({
+        mouseover:function(){
+            $(".icon-twol").show();
+        },
+        mouseout:function(){
+            $(".icon-twol").hide();
+        }
+    });
+    $("#isl").on({
+        mouseover:function(){
+            $(".icon-threel").show();
+        },
+        mouseout:function(){
+            $(".icon-threel").hide();
+        }
+    });
+
+    $("#keepa").on({
+        mouseover:function(){
+            $(".icon-onea").show();
+        },
+        mouseout:function(){
+            $(".icon-onea").hide();
+        }
+    });
+    $("#beara").on({
+        mouseover:function(){
+            $(".icon-twoa").show();
+        },
+        mouseout:function(){
+            $(".icon-twoa").hide();
+        }
+    });
+    $("#isa").on({
+        mouseover:function(){
+            $(".icon-threea").show();
+        },
+        mouseout:function(){
+            $(".icon-threea").hide();
+        }
+    });
+
+
+    $("#keepho").on({
+        mouseover:function(){
+            $(".icon-oneho").show();
+        },
+        mouseout:function(){
+            $(".icon-oneho").hide();
+        }
+    });
+    $("#bearho").on({
+        mouseover:function(){
+            $(".icon-twoho").show();
+        },
+        mouseout:function(){
+            $(".icon-twoho").hide();
+        }
+    });
+    $("#isho").on({
+        mouseover:function(){
+            $(".icon-threeho").show();
+        },
+        mouseout:function(){
+            $(".icon-threeho").hide();
+        }
+    });
+
+
+    $("#keepm").on({
+        mouseover:function(){
+            $(".icon-onem").show();
+        },
+        mouseout:function(){
+            $(".icon-onem").hide();
+        }
+    });
+    $("#bearm").on({
+        mouseover:function(){
+            $(".icon-twom").show();
+        },
+        mouseout:function(){
+            $(".icon-twom").hide();
+        }
+    });
+    $("#ism").on({
+        mouseover:function(){
+            $(".icon-threem").show();
+        },
+        mouseout:function(){
+            $(".icon-threem").hide();
+        }
+    });
+
+
+    $("#keepp").on({
+        mouseover:function(){
+            $(".icon-onep").show();
+        },
+        mouseout:function(){
+            $(".icon-onep").hide();
+        }
+    });
+    $("#bearp").on({
+        mouseover:function(){
+            $(".icon-twop").show();
+        },
+        mouseout:function(){
+            $(".icon-twop").hide();
+        }
+    });
+    $("#isp").on({
+        mouseover:function(){
+            $(".icon-threep").show();
+        },
+        mouseout:function(){
+            $(".icon-threep").hide();
+        }
+    });
+
+    $("#keepse").on({
+        mouseover:function(){
+            $(".icon-onese").show();
+        },
+        mouseout:function(){
+            $(".icon-onese").hide();
+        }
+    });
+    $("#bearse").on({
+        mouseover:function(){
+            $(".icon-twose").show();
+        },
+        mouseout:function(){
+            $(".icon-twose").hide();
+        }
+    });
+    $("#isse").on({
+        mouseover:function(){
+            $(".icon-threese").show();
+        },
+        mouseout:function(){
+            $(".icon-threese").hide();
+        }
+    });
+
+    $("#keepqi").on({
+        mouseover:function(){
+            $(".icon-oneqi").show();
+        },
+        mouseout:function(){
+            $(".icon-oneqi").hide();
+        }
+    });
+    $("#bearqi").on({
+        mouseover:function(){
+            $(".icon-twoqi").show();
+        },
+        mouseout:function(){
+            $(".icon-twoqi").hide();
+        }
+    });
+    $("#isqi").on({
+        mouseover:function(){
+            $(".icon-threeqi").show();
+        },
+        mouseout:function(){
+            $(".icon-threeqi").hide();
+        }
+    });
+
+
+})
+
+function openDialog(obj){
+	
+	$("#service").val($(obj).attr("_id"));
+	
+    $('#my_modal').show();
+    $("#leave_modal").slideToggle("slow");
+}
+
+function closeDialog(){
+    $('#my_modal').hide();
+    $("#leave_modal").slideToggle("slow");
+}
+
+function leave(){
+	
+	var sex = $('input[name="sex"]:checked').val();
+    var name = $('#name').val();
+    var iphone = $('#iphone').val();
+    var message = $('#message').val();
+    var service = $("#service").val();//不同店铺 service不同
+
+    if(!iphone || !message) {
+        alert("必填信息不能为空！");
+        return;
+    }
+	
+	$.ajax({
+        url: "http://114.215.179.175:9090/leaveMessage/save",
+        type: "POST",
+        dataType: "jsonp",
+        data: {
+            source:1,
+            service:service,
+            sex: sex,
+            name: name,
+            touch:iphone,
+            content:message
+        },
+        jsonpCallback: "__save",
+        success: function (data) {
+            alert("发送成功！");
+            $('#name').val("");
+            $('#iphone').val("");
+            $('#message').val("");
+        },
+        error :function(data){
+            console.log(data);
+        }
+    });
+}
+
+
+
+
+function openmodal(){
+    $('#black_layer').show();
+    $("#login_modal").slideToggle("slow");
+}
+
+function closemodal(){
+    $('#black_layer').hide();
+    $("#login_modal").slideToggle("slow");
+}
+function dologin(){
+    $('#loginForm').submit();
+}
